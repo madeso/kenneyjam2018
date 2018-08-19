ed390a0ef71af9f7251671faa0b50283acd8eeda
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-19 11:47:02 +0200

    added movement

--- Player.gd
+++ Player.gd
@@ -4,6 +4,8 @@ var mouse_invert_x = false
 var mouse_invert_y = false
 var mouse_sensitivity = 0.3
 
+const SPEED = 7
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
@@ -13,6 +15,27 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+func flatten(aim):
+	aim.y = 0
+	return aim.normalized()
+	
+
+func _physics_process(delta):
+	var move = Vector3()
+	var aim = $Head/Camera.global_transform.basis
+	
+	if Input.is_action_pressed("ui_up"):
+		move += flatten(-aim.z)
+	if Input.is_action_pressed("ui_down"):
+		move += flatten(aim.z)
+	if Input.is_action_pressed("ui_left"):
+		move += flatten(-aim.x)
+	if Input.is_action_pressed("ui_right"):
+		move += flatten(aim.x)
+
+	move_and_slide(move.normalized() * SPEED)
+	
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -24,9 +47,5 @@ func _input(event):
 			inverted_y = 1
 		$Head.rotate_y( inverted_x * deg2rad(event.relative.x * mouse_sensitivity) )
 		var c = $Head/Camera.rotation_degrees.x
-		c += inverted_y * event.relative.y * mouse_sensitivity
-		if c < -80:
-			c = -80
-		if c > 80:
-			c = 80
+		c = clamp(c + inverted_y * event.relative.y * mouse_sensitivity, -80, 80)
 		$Head/Camera.rotation_degrees.x = c
