38d945882d26bb3965f18d0f55a115b8469da4a8
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-19 21:40:33 +0200

    nature, grab objects and stuff

--- 3d/fix-objs.py
+++ 3d/fix-objs.py
@@ -15,7 +15,7 @@ import os
 # in assimp dir i 'mkdir makefilebuild' and in that I 'cmake ..' and 'make' to get the assimp_cmd executable
 # on windows there is a release build that might contain a prebuilt cmd, I don't know
 assimp_path = os.path.expanduser('~/dev/assimp/makefilebuild/tools/assimp_cmd/assimp')
-src_folder = os.path.expanduser('~/Downloads/kenney/3d-minigolf-pack/Models')
+# src_folder = os.path.expanduser('~/Downloads/kenney/3d-minigolf-pack/Models')
 
 for obj in glob.glob('*.obj'):
     # triangulate
@@ -24,7 +24,7 @@ for obj in glob.glob('*.obj'):
     # improve cache locality
     # fix normals
     # optimize meshes
-    from_path = os.path.join(src_folder, obj)
+    from_path = os.path.join(os.getcwd(), obj)
     to_path = os.path.join(os.getcwd(), obj)
     base = os.path.splitext(obj)[0]
     sett_file = os.path.join(os.getcwd(), base + '.align')
--- Player.gd
+++ Player.gd
@@ -13,12 +13,23 @@ const JUMP = 10
 const GRAVITY = 9.8 * 3
 const UP = Vector3(0, 1, 0)
 
+const GRAB_LENGTH = 10
+
+const HOLD_LENGTH = 3
+const GRAB_FORCE = 3
+const GRAB_LENGTH_LIM = 0.1
+const THROW_IMP = 3
+
+var grabbed_object = null
+var andamp = 0.0
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
+# func _process(delta):
+	# get_world().direct_space_state.intersect_ray($Head/Camera.global_transform.
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
@@ -29,6 +40,38 @@ func flatten(aim):
 	
 
 func _physics_process(delta):
+	var hand = false
+	if grabbed_object == null:
+		var over = null
+		var from = $Head/Camera.global_transform.origin
+		var to = from - $Head/Camera.global_transform.basis.z * GRAB_LENGTH
+		var grabbed = get_world().direct_space_state.intersect_ray(from, to, [self])
+		if grabbed.has('collider'):
+			var obj = grabbed['collider']
+			if obj is RigidBody:
+				hand = true
+				over = obj
+		if over != null and Input.is_action_just_pressed("grab_object"):
+			grabbed_object = over
+			andamp = grabbed_object.angular_damp
+			grabbed_object.gravity_scale = 0
+			grabbed_object.angular_damp = 20
+	else:
+		var target = $Head/Camera.global_transform.origin - $Head/Camera.global_transform.basis.z * HOLD_LENGTH
+		var diff = target - grabbed_object.global_transform.origin
+		if diff.length_squared() > GRAB_LENGTH_LIM:
+			grabbed_object.linear_velocity = diff * GRAB_FORCE
+		else:
+			grabbed_object.linear_velocity = Vector3(0,0,0)
+			
+		if Input.is_action_just_pressed("grab_object"):
+			grabbed_object.gravity_scale = 1
+			grabbed_object.angular_damp = andamp
+			grabbed_object = null
+	$"./../Hud/CenterContainer/Hand".visible = hand
+	
+		
+	
 	var move = Vector3()
 	var aim = $Head/Camera.global_transform.basis
 	
