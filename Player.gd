c185a9533f87b34fbf179898123398f1d3b8daa1
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-19 12:08:33 +0200

    can only jump when standing on ground

--- Player.gd
+++ Player.gd
@@ -11,6 +11,7 @@ const RUN_SPEED = 14
 const JUMP = 10
 
 const GRAVITY = 9.8 * 3
+const UP = Vector3(0, 1, 0)
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -42,14 +43,14 @@ func _physics_process(delta):
 	
 	velocity -= GRAVITY * delta
 	
-	if Input.is_action_just_pressed("move_jump"):
+	if is_on_floor() and Input.is_action_just_pressed("move_jump"):
 		velocity = JUMP
 	
 	var speed = WALK_SPEED
 	if Input.is_action_pressed("move_sprint"):
 		speed = RUN_SPEED
 	
-	velocity = move_and_slide(move.normalized() * speed + Vector3(0, velocity, 0)).y
+	velocity = move_and_slide(move.normalized() * speed + Vector3(0, velocity, 0), UP).y
 	
 	
 
